5170a47f2d003407a68f58f7ff23788480b3c88a
HOJA M A <hoja@ymail.com>
2015-04-10 17:05:10 +0530

    Delete dbconf.php

--- dbconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-$username	= 	'pits';
-$host			=	'localhost';
-$password	=	'MaranzaPits1';
-$db			=	'pits_hoja';
-$con 			=	mysql_connect($host,$username,$password) or die("Unable to connect to MySQL");
-$db_select	=	mysql_select_db($db,$con);
